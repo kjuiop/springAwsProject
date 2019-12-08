b9068b48e430443082c21ecd2bb9bfb5b0c80891
kjuiop <copying_y@naver.com>
2019-12-08 22:00:02 +0900

    start

---
+++ JavaTPC/src/main/java/com/toyproject/springboot/javatpc/BookDTO.java
@@ -0,0 +1,11 @@
+package com.toyproject.springboot.javatpc;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDTO {
+    private String title;
+    private String content;
+}
---
+++ JavaTPC/src/main/java/com/toyproject/springboot/javatpc/Project01_A.java
@@ -0,0 +1,7 @@
+package com.toyproject.springboot.javatpc;
+
+public class Project01_A {
+    public static void main(String[] args) {
+        System.out.println("in");
+    }
+}
